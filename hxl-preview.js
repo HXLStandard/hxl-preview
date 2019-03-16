0184a01396424a51cd61b9629d7c69d97010da7d
David Megginson <david.megginson@gmail.com>
2019-03-16 17:41:26 -0400

    Added working card view, and lots of other enhancements. Closer to an MVP.

--- hxl-preview.js
+++ hxl-preview.js
@@ -1,17 +1,48 @@
-hxl_preview = {};
+hxl_preview = {
+    rowIndex: 0,
+    totalRows: -1
+};
 
 /**
  * Parse GET parameters
+ * @param s: the raw query string (omitting ? or !)
  * @returns: an object mapping names to values
  */
-hxl_preview.getParams = function () {
+hxl_preview.getParams = function (s) {
     var params = {};
     var parts = [];
-    location.search.substr(1).split("&").forEach((param) => {
+
+    s.split("&").forEach((param) => {
         parts = param.split('=');
         params[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
     });
     return params;
+};
+
+/**
+ * Return a link node if appropriate.
+ */
+hxl_preview.makeText = function(value, column) {
+    if (column.attributes.indexOf("url") > -1) {
+        try {
+            new URL(value);
+            var linkNode = document.createElement("a");
+            linkNode.href = value;
+            linkNode.appendChild(document.createTextNode(value));
+            return linkNode;
+        } catch(e) {
+            console.log("Not a URL", value);
+        }
+    }
+
+    if ((column.tag == "#country" || column.tag == "#sector" || column.tag == "#subsector" || column.tag == "#org") && column.attributes.indexOf("code") == -1) {
+        var linkNode = document.createElement("a");
+        linkNode.href = "https://data.humdata.org/search?q=" + encodeURIComponent(value);
+        linkNode.appendChild(document.createTextNode(value));
+        return linkNode;
+    }
+    
+    return document.createTextNode(value);
 };
 
 /**
@@ -20,35 +51,47 @@ hxl_preview.getParams = function () {
 hxl_preview.drawCards = function (dataset) {
     var containerNode = document.getElementById("preview");
 
-    dataset.rows.forEach((row) => {
-        var sectionNode = document.createElement("section");
-        sectionNode.className = "card";
+    var cardsNode = document.createElement("div");
+    cardsNode.className = "hxl-cards";
+    containerNode.appendChild(cardsNode);
+
+    dataset.rows.forEach((row, index) => {
+        var cardNode = document.createElement("section");
+        cardNode.className = "hxl-card";
+        if (index != hxl_preview.rowIndex) {
+            cardNode.hidden = true;
+        }
+
+        hxl_preview.totalCards = index + 1;
 
         dataset.columns.forEach((column, index) => {
-            var rowNode = document.createElement("tr");
+            var fieldNode = document.createElement("div");
+            fieldNode.className = "field";
 
-            var headerNode = document.createElement("th");
+            var labelNode = document.createElement("div");
+            labelNode.className = "label";
 
-            var node = document.createElement("div");
+            var node = document.createElement("span");
             node.className = "header";
             node.appendChild(document.createTextNode(column.header));
-            headerNode.appendChild(node);
+            labelNode.appendChild(node);
 
-            var node = document.createElement("div");
+            var node = document.createElement("span");
             node.className = "hashtag";
             node.appendChild(document.createTextNode(column.displayTag));
-            headerNode.appendChild(node);
+            labelNode.appendChild(node);
             
-            rowNode.appendChild(headerNode);
+            fieldNode.appendChild(labelNode);
 
-            var valueNode = document.createElement("td");
-            valueNode.appendChild(document.createTextNode(row.values[index]));
-            rowNode.appendChild(valueNode);
+            var valueNode = document.createElement("div");
+            valueNode.className = "value";
+            valueNode.appendChild(hxl_preview.makeText(row.values[index], row.columns[index]))
+            fieldNode.appendChild(valueNode);
 
-            sectionNode.appendChild(rowNode);
+            cardNode.appendChild(fieldNode);
         });
         
-        containerNode.appendChild(sectionNode);
+        cardsNode.appendChild(cardNode);
     });
 };
 
@@ -59,7 +102,7 @@ hxl_preview.drawTable = function (dataset) {
     var containerNode = document.getElementById("preview");
 
     var tableNode = document.createElement("table");
-    tableNode.className = "hxl";
+    tableNode.className = "hxl-table";
     containerNode.appendChild(tableNode);
     
     var theadNode = document.createElement("thead");
@@ -88,9 +131,9 @@ hxl_preview.drawTable = function (dataset) {
 
     dataset.rows.forEach((row) => {
         var rowNode = document.createElement("tr");
-        row.values.forEach((value) => {
+        row.values.forEach((value, index) => {
             var cellNode = document.createElement("td");
-            cellNode.appendChild(document.createTextNode(value));
+            cellNode.appendChild(hxl_preview.makeText(row.values[index], row.columns[index]));
             rowNode.appendChild(cellNode);
         });
         tbodyNode.appendChild(rowNode)
@@ -101,16 +144,35 @@ hxl_preview.drawTable = function (dataset) {
  * Onload function
  */
 hxl_preview.load = function () {
-    var params = hxl_preview.getParams();
+
+    var localParams = {
+        style: "table"
+    };
+    
+    if (location.hash) {
+        localParams = hxl_preview.getParams(location.hash.substr(1));
+
+        // Convert to after the #
+        if (localParams.row !== undefined) {
+            hxl_preview.rowIndex = Number.parseInt(localParams.row, 10);
+            if (Number.isNaN(hxl_preview.rowIndex)) {
+                hxl_preview.rowIndex = 0;
+            }
+        }
+    }
+    
+    var params = hxl_preview.getParams(location.search.substr(1));
+
     if (params.url) {
         hxl.proxy(params.url, (dataset) => {
-            if (params.style == "cards") {
+            if (localParams.style == "cards") {
                 hxl_preview.drawCards(dataset);
             } else {
                 hxl_preview.drawTable(dataset);
             }
         });
     }
+
 };
 
 window.onload = hxl_preview.load;
